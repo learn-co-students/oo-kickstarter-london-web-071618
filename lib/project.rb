e4375371497db90b6568bd21d27b8c6970f66dd9
Ramona M <r1mari@yahoo.com>
2018-07-19 17:11:39 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+   attr_accessor :backer
+   attr_reader :title
+ 
+ def initialize(title)
+   @title = title
+  
+   @backers = []
+   
+ end
+  
+  def add_backer(backers)
+   
+end
+end 
