a35d84ec366197c8a639a0cbcb3d796e8bf712d1
github username <github email address>
2018-07-22 16:34:56 +0100

    done

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,24 @@
+require 'pry'
+require_relative './project.rb'
+
 class Backer
   attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
-    @backed_projects =[]
+    @backed_projects = []
   end
 
-  def back_project(title)
-    @title = Project.new(title)
-    @backed_projects << title
-    @title.backers << self
-  end
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
 
+  end
 end
+
+# john = Backer.new("John Smith")
+# john.back_project("My awesome project")
+#
+# binding.pry
+#
+# puts "bla"
--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,13 @@
 class Project
-  attr_reader :title, :backers
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(name)
-    @name = Backer.new(name)
-    @backers << name
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
   end
 end
